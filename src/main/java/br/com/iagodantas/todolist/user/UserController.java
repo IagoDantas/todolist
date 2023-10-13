15dc13a3e61561d97bc359e3f5e9a5f555f04f3c
Iago Dantas <iagodantas.8@gmail.com>
2023-10-12 21:08:48 -0300

    create route tasks

--- src/main/java/br/com/iagodantas/todolist/user/UserController.java
+++ src/main/java/br/com/iagodantas/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.iagodantas.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,11 @@ public class UserController {
 
              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHash = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHash);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
